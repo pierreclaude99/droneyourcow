0f978de2f432ba8c83af5243d23c2b6589445fe9
aline leroy <aline.leroy.27@gmail.com>
2020-10-13 20:52:34 +0200

    controller drones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_152116) do
+ActiveRecord::Schema.define(version: 2020_10_10_153018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,17 @@ ActiveRecord::Schema.define(version: 2020_10_10_152116) do
     t.index ["user_id"], name: "index_drones_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "delivery_id", null: false
+    t.text "content"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["delivery_id"], name: "index_reviews_on_delivery_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -60,4 +71,6 @@ ActiveRecord::Schema.define(version: 2020_10_10_152116) do
   add_foreign_key "deliveries", "drones"
   add_foreign_key "deliveries", "users"
   add_foreign_key "drones", "users"
+  add_foreign_key "reviews", "deliveries"
+  add_foreign_key "reviews", "users"
 end
