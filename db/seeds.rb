707d4f3904e21788e6b0a72e6fae5cea4ea85891
aline leroy <aline.leroy.27@gmail.com>
2020-10-13 22:22:02 +0200

    front view index drones

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "machin@bidule.com", password: "123456")
+
+Drone.create(model: "Drone 1", autonomy: 30, maximum_distance: 10, carry_capacity: 1, delivery_price: 30, user: User.first)
+Drone.create(model: "Drone 2", autonomy: 30, maximum_distance: 10, carry_capacity: 1, delivery_price: 30, user: User.first)
+Drone.create(model: "Drone 3", autonomy: 30, maximum_distance: 10, carry_capacity: 1, delivery_price: 30, user: User.first)
+Drone.create(model: "Drone 4", autonomy: 30, maximum_distance: 10, carry_capacity: 1, delivery_price: 30, user: User.first)
+Drone.create(model: "Drone 5", autonomy: 30, maximum_distance: 10, carry_capacity: 1, delivery_price: 30, user: User.first)
