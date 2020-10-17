ec21b153629138833adac17d40327595958f5ff3
Alexandre Levesque <alevek.75@gmail.com>
2020-10-17 09:47:13 +0200

    reviews' working

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,42 @@
+class ReviewsController < ApplicationController
+      before_action :set_delivery, only: [:show, :new, :create, :destroy]
+
+    def index
+      @reviews = Review.all
+    end
+
+    def show
+      @review = Review.find(params[:id])
+    end
+
+    def new
+      @review = Review.new
+    end
+
+    def create
+      @review = Review.new(params_review)
+      @review.delivery = @delivery
+      @review.user = current_user
+      if @review.save!
+        redirect_to drone_path(@delivery.drone_id)
+      else
+        render :new
+      end
+    end
+
+    def destroy
+      @review.destroy
+
+      redirect_to drone_deliveries_path(@delivery)
+    end
+
+    private
+
+    def params_review
+      params.require(:review).permit(:rating, :content)
+    end
+
+    def set_delivery
+      @delivery = Delivery.find(params[:delivery_id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :drones  do
-    resources :deliveries, only: [:create]
+    resources :deliveries, only: [:create] do
+      resources :reviews, only: [:new, :create]
+    end
   end
 end
