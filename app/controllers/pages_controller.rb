9c1fe5e412f47feed1032c0c04498c59de4a5331
ademola toukourou <tkradm@yahoo.fr>
2020-10-17 16:22:17 +0200

    user profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :profile]
   def home
+  end
+  def profile
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'drones#index'
+  get "profile/:id", to: "pages#profile", as: :profile
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :drones  do
     resources :deliveries, only: [:create] do
