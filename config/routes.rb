d5934d299cc73c33e96778838001062449d553ec
Alexandre Levesque <alevek.75@gmail.com>
2020-10-17 12:59:07 +0200

    change root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'drones#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :drones  do
     resources :deliveries, only: [:create] do
